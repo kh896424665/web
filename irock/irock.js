afdb593b2439825d90ddb4ecd0a7994db5066083
kh896424665 <896424665@qq.com>
2017-07-24 10:16:44 +0800

    stydy

--- irock/irock.js
+++ irock/irock.js
@@ -1,7 +1,28 @@
+ var username;
 function touchRock() {
-  var username = prompt("What's your name","write your name here");
-  if(username){
-    alert("It is good to meet you, "+username+".");
-    document.getElementById("rocking").src="irock.jpg";
-  }
+    if (username) {
+        alert("I like the attention, "+username+".Thank you!");
+    }
+    else {
+        username=prompt("What's your name","write your name here");
+        if (username) {
+            alert("It is good to meet you, "+username+".");
+            writeCookie("irock_username",username,5*365);
+        }
+    }
+  document.getElementById("rocking").src="irock.jpg";
+  setTimeout("document.getElementById('rocking').src='irock2.jpg'",2000);
+}
+function resizeRock() {
+    document.getElementById("rocking").style.height=(document.body.clientHeight-100)*0.9;
+}
+function greetUser() {
+    username=readCookie("irock_username");
+    if(username){
+        alert("Hello "+username+" I miss you!");
+    }
+    else {
+        alert("Hello，I am your pet rock");
+    }
+
 }
